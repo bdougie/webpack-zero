325d0666044b31ee3ffa3d13956d75f23d62720f
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-03-22 16:41:38 -0700

    step 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,10 @@
-module.exports = {}
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve('./src/index.js'),
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'bundle.js',
+    },
+};
 
